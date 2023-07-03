3f2038ddf583d7583eb871957053f349f7bccd0c
MatkovskyA <a.matkovsky14@gmail.com>
2023-07-03 10:29:54 +0300

    Вынос функции из обработчика

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const posts = [];
-
-const maxTitleLength = 100;
-
 const countOfSymbolsTitle = document.querySelector('.js__count');
 const postTitleInputNode = document.querySelector('.js__post_title-input');
 const postTextInputNode = document.querySelector('.js__post_text-input');
 const publishBtnPost = document.querySelector('.js__publish_btn');
 const postsNode = document.querySelector('.js__posts')
+
+const posts = [];
+const maxTitleLength = 100;
 
 // считаем кол-во введенных символов в заголовке
 postTitleInputNode.addEventListener('input', function(event){
@@ -17,17 +16,6 @@ postTitleInputNode.addEventListener('input', function(event){
     if (length === maxTitleLength) {
         alert('Введено максимальное количество символов для заголовка')
     }
-})
-
-// выводим значения по нажатию на кнопку
-publishBtnPost.addEventListener('click', function() {
-    const postFromUser = getPostFromUser();
-    addPost(postFromUser);
-    renderPosts();
-
-    postTitleInputNode.value = '';
-    postTextInputNode.value = '';
-    countOfSymbolsTitle.textContent = '0';
 })
 
 // получаем данные поста
@@ -92,3 +80,16 @@ function renderPosts() {
     })
     postsNode.innerHTML = postsHTML;
 }
+
+// выводим значение и обнуляем поля формы 
+function pushPostAndResetInputs() {
+    const postFromUser = getPostFromUser();
+    addPost(postFromUser);
+    renderPosts();
+
+    postTitleInputNode.value = '';
+    postTextInputNode.value = '';
+    countOfSymbolsTitle.textContent = '0';
+}
+// выводим значения через обраотчика событий click
+publishBtnPost.addEventListener('click', pushPostAndResetInputs);
